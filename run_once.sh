f3714767bb081e843d86802c2188e153a7a3ec16
Daniel M. Lofaro <dan@danlofaro.com>
2018-12-21 14:27:05 -0500

    added a run once which will run on a cron job

---
+++ run_once.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/pi/projects/the.narrative.machine.twitter/
+python /home/pi/projects/the.narrative.machine.twitter/twitter_mention_once.py
+echo "played"
--- twitter_mention_once.py
+++ twitter_mention_once.py
@@ -6,36 +6,15 @@ import sys
 import tweepy
 import time
 import os
-
-CONSUMER_KEY = '067x70cnWGAltFo8YUFIhrN6X'
-CONSUMER_SECRET = '7O9NTSm1oB7BH1hLLIe2pHFPuqm2KQmbDSUQo2yOWyn41p4jyh'
-
-ACCESS_KEY = '1052610096327524355-0nGLxfYObTplXq5lx5gwS7eT8Kf1pQ'
-ACCESS_SECRET = 'NGmpho8ZVqOPFcYuGmHgA3VZgPCwIWuzCioTRvtS5UpoP'
-
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-
-api = tweepy.API(auth)
-
+import random
 a='none'
 
 theLast = None
 ri = 0
+ri = random.randint(0,3)
 def start():
         global theLast, ri,a
-        a = 'mentions'
-	if (a == 'mentions'):
-		mymentions = api.mentions_timeline()
-                doExit = False 		
-		for tweet in mymentions:
-                     if (not doExit):   
-			d = str(unicode(tweet.text).encode("utf-8"))+" >>  "+str(unicode(tweet.user.screen_name).encode("utf-8"))
-                        ##### d is the most recent received test #####
-                        if(theLast != d):
-			  print d
-                          theLast = d
+	if True:
                           os.system("python /home/pi/projects/the.narrative.machine.twitter/runNM.py &")
                           time.sleep(25.0)
                           if ri == 0:
@@ -60,17 +39,9 @@ def start():
                           ri = ri+1
                           if ri > 3:
                             ri = 0
-                        doExit = True
+                          doExit = True
                      
 	else:
-		if (a.lower() == 'home'):
-			home = api.home_timeline()
-			for i in home:
-				print i.text + " >> " + i.user.screen_name		
-		else:
-			api.update_status(a)
+            print "no play"		
+start()
 
-di = 0
-while(1):
-	start()
-        time.sleep(30)
